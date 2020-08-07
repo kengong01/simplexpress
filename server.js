851742dad40ff4c25e15806efb05121033159849
unknown <kengong01@yahoo.com>
2020-08-07 12:39:42 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+app.listen(8080, () => {
+  console.log("Server running on port 8080");
+});
+
+app.get("/", (req, res, next) => {
+  res.status(200).send("Running!");
+});
+app.get("/health", (req, res, next) => {
+  res.status(200).send("Healthy!");
+});
