965b658c450d6266230cc001eee7262c469bec17
unknown <kengong01@yahoo.com>
2020-08-10 22:58:19 -0700

    change file, trigger cicd

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const ENV_USER = process.env.ENV_USER;
 
 app.listen(8080, () => {
   console.log("Server running on port 8080");
@@ -9,5 +10,6 @@ app.get("/", (req, res, next) => {
   res.status(200).send("Running!");
 });
 app.get("/health", (req, res, next) => {
-  res.status(200).send("${process.env.ENV_USER}, hope ur healthy?!");
+  console.log('${ENV_USER}');
+  res.status(200).send("${ENV_USER}, hope ur healthy?!");
 });
